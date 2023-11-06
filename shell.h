c352b65c832464592b55fe737ae8449187ce1d3b
kmmadu <mmadubugwukingsley@gmail.com>
2023-11-06 12:56:35 +0100

    shell.h update

--- shell.h
+++ shell.h
@@ -14,12 +14,20 @@
 
 /* Function Declarations */
 
-void _print(char *str);
-void display_prompt(void);
-void command_exec(char *cmd);
-void read_cmd(char *cmd, size_t size);
-char **tokenize_cmd(char *cmd, char *delim, int *num_tokens);
-int _strlen(char *str);
+int show_help(info_t *info)
+int change_directory(info_t *info)
+int exit_shell(info_t *info)
+void _print(const char *str)
+char **tokenize_cmd(const char *cmd, const char *delim, int *num_tokens)
+int _strlen(const char *str)
+int check_interactive(info_t *info)
+int is_delimiter(char c, char *delim)
+int is_alpha(int c)
+int custom_atoi(char *s)
+void display_prompt(void)
+void command_exec(char *cmd)
+void read_cmd(char *cmd, size_t size)
+
 
 #endif
 
